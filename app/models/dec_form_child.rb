04da4c673d46bc378e97d64b33b22ed78ca7da08
Jason <jason.addie@gmail.com>
2012-09-25 16:42:18 +0400

    added attr_accessible so form fields properly saved

--- app/models/dec_form_child.rb
+++ app/models/dec_form_child.rb
@@ -1,4 +1,3 @@
 class DecFormChild < ActiveRecord::Base
    belongs_to :dec_form
-   attr_accessible :dec_form_children_attributes
 end
--- app/models/pec_form.rb
+++ app/models/pec_form.rb
@@ -1,2 +1,16 @@
 class PecForm < ActiveRecord::Base
+
+  attr_accessible :election_district_number,
+		:election_precinct_number,
+		:observer_name,
+		:current_number_of_complaints,
+		:complaint_author,
+		:registration_time,
+		:do_you_give_complaint_to_higher_commission,
+		:violation_essence,
+		:request,
+		:was_complaint_setisfied,
+		:other_extra_comments
+
+
 end
