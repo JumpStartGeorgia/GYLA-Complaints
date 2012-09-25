0b1f0059b2fbe5791fb45212f5552d69194ea6f3
Jason <jason.addie@gmail.com>
2012-09-25 17:10:46 +0400

    made it so if date value exists, it pre-populates datatime picker

--- app/assets/javascripts/cec_forms.js
+++ app/assets/javascripts/cec_forms.js
@@ -5,19 +5,27 @@ $(function(){
 		$("input[id*='_cesko_complaint_registration_time']").live('click', function() {
 	    $(this).datetimepicker('destroy').datetimepicker({
 				showOn:'focus',
-				dateFormat: 'dd.mm.yy',
+				dateFormat: 'yy-mm-dd',
 				timeFormat: 'hh:mm',
 				separator: ' '
 			}).focus();
+			if ($(this).val() !== undefined && $(this).val().length > 0)
+			{
+				$(this).datepicker("setDate", new Date($(this).val().replace(" UTC", "")));
+			}
     });
 
 		$("input[id*='_cesko_complaint_discation_time']").live('click', function() {
 	    $(this).datetimepicker('destroy').datetimepicker({
 				showOn:'focus',
-				dateFormat: 'dd.mm.yy',
+				dateFormat: 'yy-mm-dd',
 				timeFormat: 'hh:mm',
 				separator: ' '
 			}).focus();
+			if ($(this).val() !== undefined && $(this).val().length > 0)
+			{
+				$(this).datepicker("setDate", new Date($(this).val()));
+			}
     });
   }
 
--- app/assets/javascripts/dec_forms.js
+++ app/assets/javascripts/dec_forms.js
@@ -5,19 +5,27 @@ $(function(){
 		$("input[id*='_complaint_district_registration_time']").live('click', function() {
 	    $(this).datetimepicker('destroy').datetimepicker({
 				showOn:'focus',
-				dateFormat: 'dd.mm.yy',
+				dateFormat: 'yy-mm-dd',
 				timeFormat: 'hh:mm',
 				separator: ' '
 			}).focus();
+			if ($(this).val() !== undefined && $(this).val().length > 0)
+			{
+				$(this).datepicker("setDate", new Date($(this).val()));
+			}
     });
 
 		$("input[id*='_complaint_disscusion_time']").live('click', function() {
 	    $(this).datetimepicker('destroy').datetimepicker({
 				showOn:'focus',
-				dateFormat: 'dd.mm.yy',
+				dateFormat: 'yy-mm-dd',
 				timeFormat: 'hh:mm',
 				separator: ' '
 			}).focus();
+			if ($(this).val() !== undefined && $(this).val().length > 0)
+			{
+				$(this).datepicker("setDate", new Date($(this).val()));
+			}
     });
   }
 
