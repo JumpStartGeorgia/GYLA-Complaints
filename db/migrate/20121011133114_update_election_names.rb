79ecceac9c46845709e7939f9b41da6ace0acab5
Jason <jason.addie@gmail.com>
2012-10-11 19:00:44 +0400

    associated wrong election to data

--- db/migrate/20121011133114_update_election_names.rb
+++ db/migrate/20121011133114_update_election_names.rb
@@ -1,10 +1,11 @@
 class UpdateElectionNames < ActiveRecord::Migration
   def up
     elections = Complaint::ELECTIONS
-    Complaint.where(:status_old => nil).update_all(:election => elections[elections.length - 1][1])
-    Complaint.where('status_old IS NOT NULL').update_all(:election => elections[0][1])
+    Complaint.where(:status_old => nil).update_all(:election => elections[0][1])
+    Complaint.where('status_old IS NOT NULL').update_all(:election => elections[1][1])
   end
 
   def down
+		Complaint.all.update_all(:election => nil)
   end
 end
